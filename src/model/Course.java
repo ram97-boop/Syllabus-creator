f7e3fcc1fc2b54d328833d1bc7b840188e219e64
ram97-boop <ram.lugtu97@gmail.com>
2020-09-23 15:21:17 +0200

    whitespace for test

--- src/model/Course.java
+++ src/model/Course.java
@@ -29,7 +29,7 @@ class CoursePart {
 class GradingScale {
 	HashMap<Integer,ArrayList<String>> gradingScaleMap;
 	
-	// Insert grading scales into gradingScaleMap here?
+	//Insert grading scales into gradingScaleMap here?
 	
 	
 	// Constructor
