9aefa19a411fcfece26ac69843daf8f5ea0ddcff
siek5256 <65340579+siek5256@users.noreply.github.com>
2020-09-23 12:17:21 +0200

    Add parameters to classes
    The structure of GradingScale should be changed since Map.Entry is difficult to extract from a HashMap.

--- src/model/Course.java
+++ src/model/Course.java
@@ -1,6 +1,17 @@
 package model;
 
+import java.util.*;
+
 public class Course {
+	String name;
+	double credits;
+	String code;
+	ArrayList<CoursePart> courseParts;
+	Map.Entry<Integer,ArrayList<String>> gradingScale;
+		// Placeholder, will need to be changed since Map.Entry cannot be extracted from a HashMap
+	boolean distance;
+	String language;
+	ArrayList<Goal> goal;
 	
 	public Course() {
 		
@@ -8,13 +19,27 @@ public class Course {
 }
 
 class CoursePart {
-	
+	String name;
+	double credits;
+	String examination;
+	Map.Entry<Integer,ArrayList<String>> gradingScale;
+		// Placeholder, will need to be changed since Map.Entry cannot be extracted from a HashMap
 }
 
 class GradingScale {
+	HashMap<Integer,ArrayList<String>> gradingScaleMap;
 	
+	public GradingScale() {
+		
+	}
+	
+	public Map.Entry<Integer,ArrayList<String>> getGradingSet() {
+		return null;
+	}
+	// Return type is a placeholder likewise
 }
 
 class Goal {
-	
+	String name;
+	ArrayList<CoursePart> parts;
 }
