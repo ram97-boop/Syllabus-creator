7c9a2ebc7120f424556e181f2778cf44860cd2bf
ram97-boop <ram.lugtu97@gmail.com>
2020-09-24 13:48:27 +0200

    updated gitignore to ignore .metadata/ files, re-added Goal and GradingScale java files without their directories

---
+++ src/model/GradingScale.java
@@ -0,0 +1,52 @@
+package GradingScaleAndGoal;
+
+import java.util.*;
+
+public class GradingScale {
+	
+	HashMap<Integer,ArrayList<String>> gradingScaleMap;
+	
+	private void createGradingScales() {
+		// ArrayList of A-F grading scale.
+		ArrayList<String> seven = new ArrayList<String>();
+		seven.add("A = Utmärkt");
+		seven.add("B = Mycket bra");
+		seven.add("C = Bra");
+		seven.add("D = Tillfredsställande");
+		seven.add("E = Tillräckligt");
+		seven.add("Fx = Underkänd, något mer arbete krävs");
+		seven.add("F = Underkänd, mycker mer arbete krävs");
+	
+		// ArrayList of VG-G-U
+		ArrayList<String> three = new ArrayList<String>();
+		three.add("VG");
+		three.add("G");
+		three.add("U");
+	
+		// ArrayList of G-U
+		ArrayList<String> two = new ArrayList<String>();
+		two.add("G");
+		two.add("U"); 
+	
+		// Insert grading scales into gradingScaleMap
+		gradingScaleMap.put(7, seven);
+		gradingScaleMap.put(3, three);
+		gradingScaleMap.put(2, two);
+	}
+	
+	// Constructor
+	public GradingScale() {
+		createGradingScales();
+	}
+	
+	/**
+	 * Returns one of the three grading scales
+	 * depending on the input x.
+	 * 
+	 * x has to be either 7, 3 or 2.
+	 */
+	public ArrayList<String> getGradingScale(int x) {
+		ArrayList<String> scale = gradingScaleMap.get(x);
+		return scale;
+	}
+}
