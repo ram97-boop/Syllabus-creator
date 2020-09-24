0e2dd9d1495a22c1b7a30380e9b65ddee6548bbd
glasseyesnake <mikaelstener@gmail.com>
2020-09-24 12:02:38 +0200

    Added isDistance to FirstPanel and did some work on ExaminationPanel and TeachingPanel.

--- src/View/ExaminationPanel.java
+++ src/View/ExaminationPanel.java
@@ -3,11 +3,45 @@ package View;
 import model.Course;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ExaminationPanel implements CoursePanel {
     private JPanel mainPanel;
     private JButton nextPanelButton;
     private JButton previousPanelButton;
+    private JPanel noPartsExaminationPanel;
+    private JPanel partsExaminationPanel;
+    private JCheckBox homeExamCheckBox;
+    private JCheckBox examinationOnEnglishCheckBox;
+    private JPanel notDistanceAttendancePanel;
+    private JPanel distanceAttendancePanel;
+    private JCheckBox hasAttendanceCheckBox;
+    private JPanel attendancePanel;
+    private JCheckBox hasAttendanceDistanceCheckBox;
+    private JPanel homeExamPanel;
+    private JPanel englishExaminationPanel;
+    private JPanel distanceHasAttendancePanel;
+
+    private ExaminationPanel() {
+        for (Component component : homeExamPanel.getComponents()) {
+            component.setEnabled(false);
+        }
+        for (Component component : englishExaminationPanel.getComponents()) {
+            component.setEnabled(false);
+        }
+        for (Component component : attendancePanel.getComponents()) {
+            component.setEnabled(false);
+        }
+        for (Component component : distanceAttendancePanel.getComponents()) {
+            component.setEnabled(false);
+        }
+        homeExamCheckBox.addActionListener(e -> updateHomeExamPanel());
+        examinationOnEnglishCheckBox.addActionListener(e -> updateEnglishExaminationPanel());
+        hasAttendanceCheckBox.addActionListener(e -> updateAttendancePanel());
+        hasAttendanceDistanceCheckBox.addActionListener(e -> updateDistanceHasAttendancePanel());
+    }
+
+
 
     private static final ExaminationPanel INSTANCE = new ExaminationPanel();
     public static ExaminationPanel getInstance() {return INSTANCE;}
@@ -25,6 +59,30 @@ public class ExaminationPanel implements CoursePanel {
     }
 
     public void updateView() {     }
+
+    private void updateHomeExamPanel() {
+        for (Component component : homeExamPanel.getComponents()) {
+            component.setEnabled(homeExamCheckBox.isSelected());
+        }
+    }
+
+    private void updateEnglishExaminationPanel() {
+        for (Component component : englishExaminationPanel.getComponents()) {
+            component.setEnabled(examinationOnEnglishCheckBox.isSelected());
+        }
+    }
+
+    private void updateAttendancePanel() {
+        for (Component component : attendancePanel.getComponents()) {
+            component.setEnabled(hasAttendanceCheckBox.isSelected());
+        }
+    }
+
+    private void updateDistanceHasAttendancePanel() {
+        for (Component component : distanceAttendancePanel.getComponents()) {
+            component.setEnabled(hasAttendanceCheckBox.isSelected());
+        }
+    }
 
     public void printOut(Course course) {
 
