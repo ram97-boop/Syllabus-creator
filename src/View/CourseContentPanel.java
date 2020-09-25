938049879d5873828c3124b073e3c990183706fe
glasseyesnake <mikaelstener@gmail.com>
2020-09-25 16:10:04 +0200

    Solving #26

--- src/View/CourseContentPanel.java
+++ src/View/CourseContentPanel.java
@@ -12,8 +12,6 @@ public class CourseContentPanel implements CoursePanel {
     private JPanel partsPanel;
     private JTextArea textArea1;
 
-    private JTextField nPartsField;
-    private JButton button1;
     private JPanel part1Panel;
     private JPanel part2Panel;
     private JPanel part3Panel;
@@ -22,17 +20,14 @@ public class CourseContentPanel implements CoursePanel {
     private JPanel part6Panel;
     private JComboBox<Integer> nPartsComboBox;
 
-    private final int[] possibleNParts = {1, 2, 3, 4, 5 ,6, 7, 8};
+    private final int[] possibleNParts = {0, 1, 2, 3, 4, 5 ,6, 7, 8};
     private int nParts = 0;
     private boolean consistsOfParts = false;
 
     // Constructors
     private CourseContentPanel() {
-        for (int possibleNPart : possibleNParts) {
-//            nPartsComboBox.addItem(possibleNPart);
-        }
-//        nPartsComboBox.setEditable(false);
-//        nPartsComboBox.addActionListener(e -> updatePartFields());
+        nPartsComboBox.setEditable(false);
+        nPartsComboBox.addActionListener(e -> updatePartFields());
     }
     private static final CourseContentPanel INSTANCE = new CourseContentPanel();
     public static CourseContentPanel getInstance() {return INSTANCE;}
@@ -49,8 +44,11 @@ public class CourseContentPanel implements CoursePanel {
     }
 
     public void updateView(Course course) {
+        for (int possibleNPart : possibleNParts) {
+            nPartsComboBox.addItem(possibleNPart);
+        }
         for (Component component : partsPanel.getComponents()) {
-//            component.setVisible(false);
+            component.setVisible(false);
         }
     }
 
@@ -64,7 +62,7 @@ public class CourseContentPanel implements CoursePanel {
         int i = 0;
         for (Component component : partsPanel.getComponents()) {
             component.setVisible(i < nParts);
-            component.setVisible(i >= nParts);
+//            component.setVisible(i >= nParts);
             i++;
         }
     }
