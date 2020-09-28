b18f4b99d3034a69d43b8345ce253191f8eae96a
ram97-boop <ram.lugtu97@gmail.com>
2020-09-28 14:35:58 +0200

    added attribute gradingScaleStrings and method userGetGradingScale that uses the strings in gradingScaleStrings as input.

--- src/model/GradingScale.java
+++ src/model/GradingScale.java
@@ -6,6 +6,14 @@ public class GradingScale {
 	
 	HashMap<Integer,ArrayList<String>> gradingScaleMap = new HashMap<Integer, ArrayList<String>>();
 	
+	// An array of strings that the user will see
+	// when choosing a grading scale.
+	String[] gradingScaleStrings = {
+			"7-gradig (A-F)",
+			"3-gradig (VG-U)",
+			"2-gradig (G-U)"
+	};
+	
 	private void createGradingScales() {
 		// ArrayList of A-F grading scale.
 		ArrayList<String> seven = new ArrayList<String>();
@@ -39,6 +47,7 @@ public class GradingScale {
 		createGradingScales();
 	}
 	
+	
 	/**
 	 * Returns one of the three grading scales
 	 * depending on the input x (int).
@@ -48,5 +57,26 @@ public class GradingScale {
 	public ArrayList<String> getGradingScale(int x) {
 		ArrayList<String> scale = gradingScaleMap.get(x);
 		return scale;
+	}
+	
+	
+	/**
+	 * A second method for getting a grading scale,
+	 * but this one uses the strings in gradingScaleStrings
+	 * as input.
+	 * 
+	 * @param choice
+	 * @return
+	 */
+	public ArrayList<String> userGetGradingScale(String choice) {
+		if (choice.equals(gradingScaleStrings[0])) {
+			return gradingScaleMap.get(7);
+		}
+		else if (choice.equals(gradingScaleStrings[1])) {
+			return gradingScaleMap.get(3);
+		}
+		else if (choice.equals(gradingScaleStrings[2])) {
+			return gradingScaleMap.get(2);
+		}
 	}
 }
