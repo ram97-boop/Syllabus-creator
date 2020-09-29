05d026435f7d1aa17fa40baa97d0882dab088fb2
glasseyesnake <mikaelstener@gmail.com>
2020-09-29 11:18:52 +0200

    Adding getter to isConnectedToAll

--- src/View/ExpectedResultPanel.java
+++ src/View/ExpectedResultPanel.java
@@ -106,6 +106,11 @@ public class ExpectedResultPanel implements CoursePanel {
     }
 
     // Getters to Controller
+
+    public JCheckBox getIsConnectedToAll() {
+        return isConnectedToAll;
+    }
+
     public JPanel[] getGoalPanels() {
         return new JPanel[]{
                 goalPanel1,
