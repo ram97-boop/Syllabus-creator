1e1f7b410c4298cb3abc8292ae578284b12f0f81
soffik <soffik1996@gmail.com>
2020-09-30 10:59:59 +0200

    Fix error in controller and test class

--- src/controller/ExpectedResultController.java
+++ src/controller/ExpectedResultController.java
@@ -8,6 +8,7 @@ import model.Goal;
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ExpectedResultController implements CourseController {
@@ -32,18 +33,17 @@ public class ExpectedResultController implements CourseController {
         ArrayList<CoursePart> courseParts = course.getCourseParts();
         ArrayList<Goal> goalList = new ArrayList<>();
 
-        JPanel[] goalPanels = expectedResultPanel.getGoalPanels();
+        HashMap<JTextField, JRadioButton[]> goalsMap = expectedResultPanel.getGoals();
 
-        Arrays.stream(goalPanels).forEach(goalPanel -> {
+        goalsMap.forEach((key, value) -> {
             Goal goal = new Goal();
-            JTextField goalDescription = (JTextField) goalPanel.getComponent(0);
 
-            if (!goalDescription.getText().isEmpty()) {
+            if (!key.getText().isEmpty()) {
 
-                goal.describeGoal(goalDescription.getText());
+                goal.describeGoal(key.getText());
 
                 if (!expectedResultPanel.getIsConnectedToAll().isSelected() && !courseParts.isEmpty()) {
-                    JPanel buttonPanel = (JPanel) goalPanel.getComponent(1);
+                    JRadioButton[] buttonPanel = value;
                     setCoursePartForGoal(buttonPanel, goal);
                 } else if (!courseParts.isEmpty()) {
                     goal.setCourseParts(courseParts);
@@ -58,18 +58,16 @@ public class ExpectedResultController implements CourseController {
 
     }
 
-    private void setCoursePartForGoal(JPanel buttonPanel, Goal goal) {
+    private void setCoursePartForGoal(JRadioButton[] buttonPanel, Goal goal) {
         ArrayList<CoursePart> courseParts = course.getCourseParts();
         AtomicInteger index = new AtomicInteger();
 
-        Arrays.stream(buttonPanel.getComponents()).forEach(component -> {
-            JRadioButton button = (JRadioButton) component;
-
+        Arrays.stream(buttonPanel).forEach(button -> {
             if (index.get() <= courseParts.size() && button.isVisible() && button.isSelected()) {
                 goal.addCoursePart(courseParts.get(index.get()));
             }
-
             index.getAndIncrement();
         });
     }
+
 }
