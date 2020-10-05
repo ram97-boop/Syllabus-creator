53e1516b74a32ee1382e6ff51bc545be618877d8
siek5256 <65340579+siek5256@users.noreply.github.com>
2020-10-05 11:15:40 +0200

    FileManagement first draft
    Class for saving and loading courses.

---
+++ src/model/FileManagement.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.io.*;
+import java.nio.*;
+import com.google.gson.*;
+
+class FileManagement {
+	
+	public void saveCourse(String filename) {
+		
+	}
+	
+	public Course loadCourse(String filename) {
+		return null;
+	}
+}
