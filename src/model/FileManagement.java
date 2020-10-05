d1c05ad741af8356002c362fd4d808b9d05f0e75
siek5256 <65340579+siek5256@users.noreply.github.com>
2020-10-05 11:34:02 +0200

    Class commentated

--- src/model/FileManagement.java
+++ src/model/FileManagement.java
@@ -4,14 +4,28 @@ import java.io.*;
 import java.nio.file.*;
 import com.google.gson.*;
 
+// Responsible for saving courses and loading courses from files.
+
 class FileManagement {
 	Gson gson = new Gson();
 	
+	/**
+	 * Saves a course to a file.
+	 * @param course - Course to save to a file.
+	 * @param filename - Name of the target file.
+	 * @throws IOException
+	 */
 	public void saveCourse(Course course,String filename) throws IOException {
 		String json = gson.toJson(course);
 		Files.write(Paths.get(filename),json.getBytes());
 	}
 	
+	/**
+	 * Reads in a course from a file.
+	 * @param filename - Name of the target file.
+	 * @return A course object.
+	 * @throws IOException
+	 */
 	public Course loadCourse(String filename) throws IOException {
 		BufferedReader reader = new BufferedReader(new FileReader(filename));
 		Course course = gson.fromJson(reader.readLine(),Course.class);
