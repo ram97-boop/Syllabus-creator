6ac826a5faed6ad24456b659ee6fb0920f0c4176
siek5256 <65340579+siek5256@users.noreply.github.com>
2020-10-05 11:20:58 +0200

    saveCourse implemented

--- src/model/FileManagement.java
+++ src/model/FileManagement.java
@@ -2,12 +2,17 @@ package model;
 
 import java.io.*;
 import java.nio.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import com.google.gson.*;
 
 class FileManagement {
+	Gson gson = new Gson();
 	
-	public void saveCourse(String filename) {
-		
+	public void saveCourse(Course course,String filename) throws IOException {
+		String json = gson.toJson(course);
+		Files.write(Paths.get(filename),json.getBytes());
 	}
 	
 	public Course loadCourse(String filename) {
