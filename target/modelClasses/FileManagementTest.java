18c579de677943f358c96efe53d912d54b1a67a4
ram97-boop <ram.lugtu97@gmail.com>
2020-10-06 15:59:58 +0200

    Added new class, FileManagementTest.

---
+++ target/modelClasses/FileManagementTest.java
@@ -0,0 +1,16 @@
+package modelClasses;
+
+import static org.junit.jupiter.api.Assertions.*;
+import model.FileManagement;
+import org.junit.jupiter.api.Test;
+import model.*;
+
+public class FileManagementTest {
+	Course c = new Course();
+	
+	@Test
+	void saveCourseTest() {
+		
+	}
+	
+}
