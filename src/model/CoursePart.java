9f00dda09f580c8a64e74227e27f8c1b9b58c9d2
siek5256 <65340579+siek5256@users.noreply.github.com>
2020-10-12 11:11:24 +0200

    Add decidesTotalGrade w/ getter and setter

--- src/model/CoursePart.java
+++ src/model/CoursePart.java
@@ -9,6 +9,7 @@ public class CoursePart {
 	String partCode; // Optional code of the course part
 	String examination; // The method of examination used in this course part
 	ArrayList<String> gradingScale; // 7-grade, 3-grade or 2-grade scale used for this course part
+	boolean decidesTotalGrade; // Whether the parent Course's total grade is affected by this course part
 	
 	public CoursePart() {
 		
@@ -42,6 +43,10 @@ public class CoursePart {
 		return gradingScale;
 	}
 	
+	public boolean getDecidesTotalGrade() {
+		return decidesTotalGrade;
+	}
+	
 	// Setters
 	
 	public void setName(String name) {
@@ -66,5 +71,9 @@ public class CoursePart {
 	
 	public void setGradingScale(ArrayList<String> gradingScale) {
 		this.gradingScale = gradingScale;
+	}
+	
+	public void setDecidesTotalGrade(boolean decidesTotalGrade) {
+		this.decidesTotalGrade = decidesTotalGrade;
 	}
 }
