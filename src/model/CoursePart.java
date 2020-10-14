2e40b66fc4a2066ce7ff03749d2c1b325c0a1032
siek5256 <65340579+siek5256@users.noreply.github.com>
2020-10-14 12:20:55 +0200

    partCode removed

--- src/model/CoursePart.java
+++ src/model/CoursePart.java
@@ -6,7 +6,6 @@ public class CoursePart {
 	String name; // Name of this course part
 	String engName; // English name of this course part
 	double credits; // Credits tied to this course part
-	String partCode; // Optional code of the course part
 	String examination; // The method of examination used in this course part
 	ArrayList<String> gradingScale; // 7-grade, 3-grade or 2-grade scale used for this course part
 	boolean decidesTotalGrade; // Whether the parent Course's total grade is affected by this course part
@@ -29,10 +28,6 @@ public class CoursePart {
 	
 	public double getCredits() {
 		return credits;
-	}
-	
-	public String getPartCode() {
-		return partCode;
 	}
 	
 	public String getExamination() {
@@ -59,10 +54,6 @@ public class CoursePart {
 	
 	public void setCredits(double credits) {
 		this.credits = credits;
-	}
-	
-	public void setPartCode(String partCode) {
-		this.partCode = partCode;
 	}
 	
 	public void setExamination(String examination) {
