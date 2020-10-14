90659f0e1668d323a80847a68c9f9a0ed34e7295
siek5256 <65340579+siek5256@users.noreply.github.com>
2020-10-14 12:31:33 +0200

    Removed partCode tests

---
+++ target/modelClasses/CoursePartTest.java
@@ -0,0 +1,38 @@
+package modelClasses;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+import org.junit.jupiter.api.Test;
+import model.*;
+import org.junit.jupiter.api.BeforeAll;
+
+public class CoursePartTest {
+	
+	static CoursePart cp = new CoursePart();
+	
+	@BeforeAll
+	static void init() {
+		cp.setName("kursDelNamn");
+		cp.setEngName("coursePartName");
+		cp.setCredits(2.5);
+		cp.setExamination("examinationTestString");
+		ArrayList<String> g = new ArrayList<String>();
+		g.add("gradingScaleTestString");
+		cp.setGradingScale(g);
+		cp.setDecidesTotalGrade(true);
+	}
+	
+	
+	@Test
+	void testSettersAndGetters() {
+		
+		assertTrue(
+				cp.getName().equals("kursDelNamn") &&
+				cp.getEngName().equals("coursePartName") &&
+				cp.getCredits() == 2.5 &&
+				cp.getExamination().equals("examinationTestString") &&
+				cp.getGradingScale().get(0).equals("gradingScaleTestString") &&
+				cp.getDecidesTotalGrade() == true);
+	}
+
+}
