ec1cd1a42a534428f5bd3dccefede8ebd6e45d5c
glasseyesnake <mikaelstener@gmail.com>
2020-10-18 13:58:06 +0200

    Commenting ExpectedResultPanel

--- src/View/ExpectedResultPanel.java
+++ src/View/ExpectedResultPanel.java
@@ -298,6 +298,7 @@ public class ExpectedResultPanel implements CoursePanel {
     public String getFrameName() {
         return properties.getProperty("ExpectedResultsTitle");
     }
+
     /**
      * Returns the JSplitPane of this panel.
      * @return
