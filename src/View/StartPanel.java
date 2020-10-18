305675d7ddb80e7a95136dc038f497e13c7cb1a9
glasseyesnake <mikaelstener@gmail.com>
2020-10-18 17:10:52 +0200

    Commenting StartPanel

--- src/View/StartPanel.java
+++ src/View/StartPanel.java
@@ -3,6 +3,9 @@ package View;
 import javax.swing.*;
 
 public class StartPanel {
+    /**
+     * Swing components
+     */
     private JPanel mainPanel;
     private JRadioButton createNewCourseButton;
     private JRadioButton continueCourseButton;
@@ -10,8 +13,30 @@ public class StartPanel {
     private JTextField courseCode;
     private JPanel courseInfoPanel;
 
+    /**
+     * Constructor
+     */
+
     public StartPanel() {
+        setupComponents();
+        addActionListeners();
+    }
+
+    /**
+     * Helper methods for constructor
+     */
+
+    /**
+     * Set initial states of components.
+     */
+    private void setupComponents() {
         courseInfoPanel.setVisible(false);
+    }
+
+    /**
+     * Add action listeners to components.
+     */
+    private void addActionListeners() {
         continueCourseButton.addActionListener(e -> {
             createNewCourseButton.setSelected(false);
             updateCourseInfoPanel();
@@ -22,16 +47,40 @@ public class StartPanel {
         });
     }
 
+    /**
+     * Methods from interface CoursePanel
+     */
+
+    /**
+     * Returns JPanel with all content.
+     * @return
+     */
     public JPanel getPanel() {
         return mainPanel;
     }
+
+    /**
+     * Returns next panel button.
+     * @return
+     */
     public JButton getNextPanelButton() {
         return nextPanelButton;
     }
 
+    /**
+     * Action listener methods
+     */
+
+    /**
+     * Shows courseInfoPanel based on if user wants to load course
+     */
     private void updateCourseInfoPanel() {
         courseInfoPanel.setVisible(continueCourseButton.isSelected());
     }
+
+    /**
+     * Getters
+     */
 
     public JRadioButton getCreateNewCourseButton() {
         return createNewCourseButton;
